dddfc6a62d3969da5836cb7e072fbd29de7febc7
Camilo Lopez <camilino123@gmail.com>
2019-04-16 20:43:39 -0500

    Login view done, login in progress

--- app/src/main/java/com/eci/cosw/taskplanner/LaunchActivity.java
+++ app/src/main/java/com/eci/cosw/taskplanner/LaunchActivity.java
@@ -1,14 +1,26 @@
 package com.eci.cosw.taskplanner;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class LaunchActivity extends AppCompatActivity {
 
+    public static final String TOKEN_KEY = "TOKEN_KEY";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launch);
+        SharedPreferences sharedPref =
+                getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        if (sharedPref.contains(TOKEN_KEY)) {
+            //TODO go to MainActivity
+        } else {
+            //TODO go to LoginActivity
+        }
     }
 }
